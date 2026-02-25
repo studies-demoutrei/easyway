68d2fde0baf1b466270cd9c38a807c38746b9c27
studies-demoutrei <studies.demoutrei@gmail.com>
2026-02-25 16:36:11 +0800

    Added /terms page

--- terms/index.js
+++ terms/index.js
@@ -5,9 +5,9 @@ const bannerImage = document.querySelector("#banner");
 
 
 bannerImage.addEventListener("click", (_) => {
-  window.open('./', "_self");
+  window.open('../', "_self");
 })
 
 
-navigationBarLogInElement.addEventListener("click", (_) => window.open('./login', "_self"));
-navigationBarSignUpElement.addEventListener("click", (_) => window.open('./signup', "_self"));
+navigationBarLogInElement.addEventListener("click", (_) => window.open('../login', "_self"));
+navigationBarSignUpElement.addEventListener("click", (_) => window.open('../signup', "_self"));
